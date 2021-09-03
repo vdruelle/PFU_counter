aeaa6e3e513a030fa354c17b9c5ca2f0ced612e8
Valentin <valentin.druelle@unibas.ch>
2021-09-03 11:22:18 +0200

    Validation error computation for plate element detection

--- scripts/main.py
+++ scripts/main.py
@@ -2,90 +2,176 @@ import os
 import sys
 import pathlib
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 import torch
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torch.utils.tensorboard import SummaryWriter
+from torch.nn.functional import l1_loss
 
 from dataset import LabDataset, LabH5Dataset
 from transforms import RandomHorizontalFlip, Compose, GaussianBlur
+from utils import plot_image_target
 
 
 def collate_fn(batch):
     return tuple(zip(*batch))
 
 
-# use the GPU if is_available
-device = torch.device('cuda:0' if torch.cuda.is_available() else print("GPU not available"))
-writer = SummaryWriter('runs/Default')
+def train_plate_detection():
+    """
+    Train a FasterRCNN to do plate element detection using the LabH5Dataset.
+    """
+    device = torch.device('cuda:0' if torch.cuda.is_available() else print("GPU not available"))
+    writer = SummaryWriter('runs/Default')
 
-dataset_folder = "data/phage_plates/"
-plate_dataset = {}
-for phase in ["train", "valid"]:
-    plate_dataset[phase] = LabH5Dataset(dataset_folder + phase + ".h5", None)
+    dataset_folder = "data/phage_plates/"
+    plate_dataset = {}
+    for phase in ["train", "valid"]:
+        plate_dataset[phase] = LabH5Dataset(dataset_folder + phase + ".h5", None)
 
-dataloader = {}
-for phase in ["train", "valid"]:
-    dataloader[phase] = torch.utils.data.DataLoader(
-        plate_dataset[phase], batch_size=4, num_workers=4, shuffle=False, collate_fn=collate_fn)
+    dataloader = {}
+    for phase in ["train", "valid"]:
+        dataloader[phase] = torch.utils.data.DataLoader(
+            plate_dataset[phase], batch_size=4, num_workers=4, shuffle=False, collate_fn=collate_fn)
 
-# The model
-# model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
-num_classes = 4  # background + the 3 others
-in_features = model.roi_heads.box_predictor.cls_score.in_features
-# replacing the pre rtained head with a new one
-model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-model.to(device)
+    # The model
+    # model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
+    num_classes = 4  # background + the 3 others
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replacing the pre rtained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    model.to(device)
 
-# Optimizer
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9, weight_decay=5e-4)
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    # Optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-2, momentum=0.9, weight_decay=5e-4)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
-
-num_epochs = 15
-for epoch in range(num_epochs):
-    print(f"--- Epoch {epoch} ---")
-    # Train
-    model.train()
-    for ii, (images, targets) in enumerate(dataloader["train"]):
-        images = list(image.to(device) for image in images)
-        targets = [{key: t[key].to(device) for key in t.keys()} for t in targets]
-
-        losses_dict = model(images, targets)
-        loss_sum = sum(loss for loss in losses_dict.values())
-
-        # Writting to tensorboard
-        for key in losses_dict.keys():
-            writer.add_scalar("Losses/" + key, losses_dict[key].item(), ii)
-        writer.add_scalar("Total_loss/train", loss_sum.item(), ii)
-
-        if not math.isfinite(loss_sum):
-            print("Loss is {}, stopping training".format(loss_sum.item()))
-            sys.exit(1)
-        else:
-            print(f"Image: {ii}    Loss: {loss_sum.item()}")
-
-        optimizer.zero_grad()
-        loss_sum.backward()
-        optimizer.step()
-
-    # Test
-    with torch.no_grad():
-        test_loss = 0
-        for images, targets in dataloader["valid"]:
+    num_epochs = 40
+    n_iter = 0
+    for epoch in range(num_epochs):
+        print(f"--- Epoch {epoch} ---")
+        # Train
+        model.train()
+        for ii, (images, targets) in enumerate(dataloader["train"]):
             images = list(image.to(device) for image in images)
             targets = [{key: t[key].to(device) for key in t.keys()} for t in targets]
 
             losses_dict = model(images, targets)
             loss_sum = sum(loss for loss in losses_dict.values())
-            test_loss += loss_sum.item()
 
-        test_loss = test_loss / len(plate_dataset["valid"])
-        writer.add_scalar("Total_loss/test", test_loss, epoch)
+            # Writting to tensorboard
+            for key in losses_dict.keys():
+                writer.add_scalar("Losses/" + key, losses_dict[key].item(), n_iter)
+            writer.add_scalar("Total_loss/train", loss_sum.item(), n_iter)
 
-    # update the learning rate
-    lr_scheduler.step()
+            if not math.isfinite(loss_sum):
+                print("Loss is {}, stopping training".format(loss_sum.item()))
+                sys.exit(1)
+            else:
+                print(f"Batch: {ii}    Loss: {loss_sum.item()}")
 
-print("That's it!")
+            optimizer.zero_grad()
+            loss_sum.backward()
+            optimizer.step()
+            n_iter += 1
+
+        # Test
+        with torch.no_grad():
+            test_loss = 0
+            model.eval()
+            for images, targets in dataloader["valid"]:
+                images = list(image.to(device) for image in images)
+                targets = [{key: t[key].to(device) for key in t.keys()} for t in targets]
+
+                outputs = model(images)
+                error = compute_validation_errors(outputs, targets)
+
+                loss_sum = sum(loss for loss in losses_dict.values())
+                test_loss += loss_sum.item()
+
+            test_loss = test_loss / len(plate_dataset["valid"])
+            writer.add_scalar("Total_loss/test", test_loss, epoch)
+
+        lr_scheduler.step()
+
+    torch.save(model.state_dict(), "model_saves/Plate_detection.pt")
+    print("That's it!")
+
+
+def predict_plate():
+    """
+    Function to test the predictions of the network trained by train_plate_detection().
+    """
+    device = torch.device('cuda:0' if torch.cuda.is_available() else print("GPU not available"))
+
+    dataset_folder = "data/phage_plates/"
+    plate_dataset = {}
+    for phase in ["train", "valid"]:
+        plate_dataset[phase] = LabH5Dataset(dataset_folder + phase + ".h5", None)
+
+    dataloader = {}
+    for phase in ["train", "valid"]:
+        dataloader[phase] = torch.utils.data.DataLoader(
+            plate_dataset[phase], batch_size=1, num_workers=1, shuffle=False, collate_fn=collate_fn)
+
+    # The model
+    # model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
+    num_classes = 4  # background + the 3 others
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replacing the pre rtained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    model.to(device)
+    model.load_state_dict(torch.load("model_saves/Plate_detection.pt"))  # Loading the trained model
+
+    model.eval()
+    with torch.no_grad():
+        # for images, targets in dataloader["valid"]:
+        images, targets = iter(dataloader["valid"]).next()
+        images = list(image.to(device) for image in images)
+
+        outputs = model(images)
+        outputs = [{k: v.cpu() for k, v in t.items()} for t in outputs]
+        # plot_image_target(images[0], outputs[0])
+        error = compute_validation_errors(outputs, targets)
+        breakpoint()
+
+        # plt.show()
+
+
+def compute_validation_errors(predictions, targets):
+    """
+    Computes a simple validation error and returns it. Basically it takes every prediction and computes an L1
+    loss to the corresponding target, weighted by the attributed score.
+    I could restrain this to only take the best prediction for Plate name and Phage names.
+    """
+    error = 0
+    for prediction, target in zip(predictions, targets):
+        for ii in range(len(prediction["scores"])):
+            # Error for plate name
+            if prediction["labels"][ii] == 1:  # plate name label id
+                tbox = target["boxes"][target["labels"] == 1][0]
+                weight = prediction["scores"][ii]
+                error += l1_loss(prediction["boxes"][ii], tbox) * weight
+            # Error for phage names
+            if prediction["labels"][ii] == 2:  # phage names label id
+                tbox = target["boxes"][target["labels"] == 2][0]
+                weight = prediction["scores"][ii]
+                error += l1_loss(prediction["boxes"][ii], tbox) * weight
+            # Error for phage columns
+            if prediction["labels"][ii] == 3:  # phage columns label id
+                tboxes = target["boxes"][target["labels"] == 3]
+                pred = prediction["boxes"][ii]
+                # Handwritten L1 loss because it does not broadcast to all targets
+                losses = torch.abs(tboxes - pred).sum(dim=1)
+                weight = prediction["scores"][ii]
+                error += losses.min() * weight  # select the target column that corresponds most
+    return error
+
+
+if __name__ == '__main__':
+    # train_plate_detection()
+    predict_plate()
