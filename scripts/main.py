dcf7616cb5942f373e29aa60b55d953166563747
Valentin <valentin.druelle@unibas.ch>
2021-10-12 16:33:58 +0200

    added optimizer function

--- scripts/main.py
+++ scripts/main.py
@@ -24,18 +24,18 @@ def train_plate_detection():
     Train a FasterRCNN to do plate element detection using the LabH5Dataset.
     """
     device = torch.device('cuda:0' if torch.cuda.is_available() else print("GPU not available"))
-    writer = SummaryWriter('runs/PlateDetector_Albu_4')
+    writer = SummaryWriter('runs/PlateDetector_Albu_6')
 
     dataset_folder = "data/phage_plates/"
     plate_dataset = {}
     for phase in ["train", "valid"]:
         plate_dataset[phase] = LabH5Dataset(dataset_folder + phase + ".h5",
-                                            PlateAlbumentation(4) if phase == "train" else None)
+                                            PlateAlbumentation(7) if phase == "train" else None)
 
     dataloader = {}
     for phase in ["train", "valid"]:
         dataloader[phase] = torch.utils.data.DataLoader(
-            plate_dataset[phase], batch_size=4, num_workers=6,
+            plate_dataset[phase], batch_size=4, num_workers=4,
             shuffle=(phase == "train"), collate_fn=collate_fn)
 
     # The model
@@ -91,7 +91,84 @@ def train_plate_detection():
 
         lr_scheduler.step()
 
-    # torch.save(model.state_dict(), "model_saves/Plate_detection.pt")
+    torch.save(model.state_dict(), "model_saves/Plate_detection.pt")
+    print("That's it!")
+
+
+def optimize_plate_detection():
+    """
+    Train a FasterRCNN to do plate element detection using the LabH5Dataset.
+    """
+    device = torch.device('cuda:0' if torch.cuda.is_available() else print("GPU not available"))
+    writer = SummaryWriter('runs/PlateDetector_optimized_5')
+
+    dataset_folder = "data/phage_plates/"
+    plate_dataset = {}
+    for phase in ["train", "valid"]:
+        plate_dataset[phase] = LabH5Dataset(dataset_folder + phase + ".h5",
+                                            PlateAlbumentation(5) if phase == "train" else None)
+
+    dataloader = {}
+    for phase in ["train", "valid"]:
+        dataloader[phase] = torch.utils.data.DataLoader(
+            plate_dataset[phase], batch_size=4, num_workers=4,
+            shuffle=(phase == "train"), collate_fn=collate_fn)
+
+    # The model
+    model = PlateDetector(num_classes=4, backbone="mobilenet", trainable_backbone_layers=None)
+    model.to(device)
+    model.load_state_dict(torch.load("model_saves/Plate_detection.pt"))
+
+    # Optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=5e-4)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1)
+
+    num_epochs = 25
+    n_iter = 0
+    for epoch in range(num_epochs):
+        print(f"--- Epoch {epoch} ---")
+        # Train
+        model.train()
+        for ii, (images, targets) in enumerate(dataloader["train"]):
+            images = list(image.to(device) for image in images)
+            targets = [{key: t[key].to(device) for key in t.keys()} for t in targets]
+
+            losses_dict = model(images, targets)
+            loss_sum = sum(loss for loss in losses_dict.values())
+
+            # Writting to tensorboard
+            for key in losses_dict.keys():
+                writer.add_scalar("Losses/" + key, losses_dict[key].item(), n_iter)
+            writer.add_scalar("Total_loss/train", loss_sum.item(), n_iter)
+
+            if not math.isfinite(loss_sum):
+                print("Loss is {}, stopping training".format(loss_sum.item()))
+                sys.exit(1)
+            else:
+                print(f"Batch: {ii}    Loss: {loss_sum.item()}")
+
+            optimizer.zero_grad()
+            loss_sum.backward()
+            optimizer.step()
+            n_iter += 1
+
+        # Test
+        with torch.no_grad():
+            valid_loss = 0
+            model.eval()
+            for images, targets in dataloader["valid"]:
+                images = list(image.to(device) for image in images)
+                targets = [{key: t[key].to(device) for key in t.keys()} for t in targets]
+
+                outputs = model(images)
+                valid_loss += compute_validation_errors(outputs, targets)
+
+            valid_loss /= len(plate_dataset["valid"])
+            writer.add_scalar("Total_loss/test", valid_loss, epoch)
+
+        lr_scheduler.step()
+
+    # torch.save(model.state_dict(), "model_saves/Optimized.pt")
     print("That's it!")
 
 
@@ -194,5 +271,6 @@ def compute_validation_errors(predictions, targets):
 
 
 if __name__ == '__main__':
-    train_plate_detection()
-    # predict_plate()
+    # train_plate_detection()
+    # optimize_plate_detection()
+    predict_plate()
