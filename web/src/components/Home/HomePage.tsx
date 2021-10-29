cbc80c224b784e7aaaa172742ab1f81e7ceaa94f
ivan-aksamentov <ivan.aksamentov@gmail.com>
2021-10-29 20:13:22 +0200

    feat: load image to the canvas

--- web/src/components/Home/HomePage.tsx
+++ web/src/components/Home/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState, useRef } from 'react'
 
 import { Col, Container, Row } from 'reactstrap'
 
@@ -10,11 +10,35 @@ export async function importAndRunModel() {
   return runModel()
 }
 
+export async function readImage(file: File, canvas: HTMLCanvasElement | null) {
+  if (!canvas) {
+    throw new Error('Canvas element is not ready')
+  }
+
+  const ctx = canvas.getContext('2d')
+  if (!ctx) {
+    throw new Error('Cannot get canvas context')
+  }
+
+  ctx.fillStyle = 'black'
+  const url = URL.createObjectURL(file)
+  const img = new Image()
+  img.addEventListener('load', () => {
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    canvas.width = img.width
+    canvas.height = img.height
+    ctx.drawImage(img, 0, 0, img.width, img.height)
+    URL.revokeObjectURL(img.src)
+  })
+  img.src = url
+}
+
 export function HomePage() {
   const [result, setResult] = useState<ModelResult>()
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
   const onUpload = useCallback((file: File) => {
-    console.warn('Not implemented: onUpload:', { file })
+    void readImage(file, canvasRef?.current)
   }, [])
 
   useEffect(() => {
@@ -34,6 +58,12 @@ export function HomePage() {
 
       <Row noGutters>
         <Col>{result && JSON.stringify(result, null, 2)}</Col>
+      </Row>
+
+      <Row noGutters>
+        <Col>
+          <canvas id="canvas" ref={canvasRef} width={200} height={200} />
+        </Col>
       </Row>
     </Container>
   )
