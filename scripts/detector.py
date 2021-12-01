2c4846686a9af2ff53024d8761b31d477ef5dc48
Valentin <valentin.druelle@unibas.ch>
2021-12-01 14:29:57 +0100

    added early save of models

--- scripts/detector.py
+++ scripts/detector.py
@@ -22,10 +22,10 @@ def train_plate_detection():
     Train a FasterRCNN to do plate element detection using the LabH5Dataset.
     """
     device = torch.device('cuda:0' if torch.cuda.is_available() else print("GPU not available"))
-    writer = SummaryWriter('runs/Plate_detector_new')
+    writer = SummaryWriter('runs/Plate_detector')
 
-    dataset_folder = {"train": "data/plates_labeled/train_full/",
-                      "test": "data/plates_labeled/test_full/"}
+    dataset_folder = {"train": "data/plates_labeled/train/",
+                      "test": "data/plates_labeled/test/"}
     plate_dataset = {}
     for phase in ["train", "test"]:
         plate_dataset[phase] = BoxDataset(dataset_folder[phase],
@@ -34,19 +34,19 @@ def train_plate_detection():
     dataloader = {}
     for phase in ["train", "test"]:
         dataloader[phase] = torch.utils.data.DataLoader(
-            plate_dataset[phase], batch_size=4, num_workers=4, shuffle=(phase == "train"),
+            plate_dataset[phase], batch_size=4, num_workers=3, shuffle=(phase == "train"),
             collate_fn=collate_fn)
 
     # The model
     model = PlateDetector(num_classes=4, backbone="mobilenet", trainable_backbone_layers=None)
     model.to(device)
-    # model.load_state_dict(torch.load("model_saves/Plate_detector.pt"))
+    model.load_state_dict(torch.load("model_saves/Plate_detector.pt"))
 
     # Optimizer
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=5e-4)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=25, gamma=0.1)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20,  gamma=0.1)
 
-    num_epochs = 30
+    num_epochs = 20
     n_iter = 0
     for epoch in range(num_epochs):
         print(f"--- Epoch {epoch} ---")
@@ -77,6 +77,7 @@ def train_plate_detection():
 
         # Test
         # model.eval()
+        min_loss = 1e10
         with torch.no_grad():
             valid_loss = 0
             for images, targets in dataloader["test"]:
@@ -92,9 +93,12 @@ def train_plate_detection():
             valid_loss /= len(plate_dataset["test"])
             writer.add_scalar("Total_loss/test", valid_loss, epoch)
 
+            if n_iter > 10 and valid_loss < min_loss:
+                torch.save(model.state_dict(), "model_saves/Plate_detector.pt")
+                min_loss = min(valid_loss, min_loss)
+
         lr_scheduler.step()
 
-    torch.save(model.state_dict(), "model_saves/Plate_detector.pt")
     print("That's it!")
 
 
@@ -200,8 +204,8 @@ def test_onnx(model_path, input_path):
 
 if __name__ == '__main__':
     # train_plate_detection()
-    # predict_full_dataset("model_saves/Plate_detector_new.pt", "data/plates_labeled/to_label/images/",
-    #                      "data/plates_labeled/to_label/labels/", show=False)
+    # predict_full_dataset("model_saves/Plate_detector_new.pt", "data/plates_labeled/test/images/",
+    #                      "data/plates_labeled/test/labels/", show=True)
     # export_to_onnx("model_saves/Plate_detector.pt", "model_saves/Plate_detector.onxx")
-    export_to_onnx("model_saves/Plate_detector.pt", "test.onnx")
+    # export_to_onnx("model_saves/Plate_detector.pt", "test.onnx")
     # test_onnx("model_saves/Plate_detector.onnx", "data/plates_labeled/test/images/20211112_103710.jpg")
